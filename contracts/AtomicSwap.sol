909c1ea5ad0d66689ca0d6a4cee0d7efdcac2802
StuartFarmer <email@stuartfarmer.me>
2018-02-12 17:05:57 -0800

    multiple swaps on a single contract

--- contracts/AtomicSwap.sol
+++ contracts/AtomicSwap.sol
@@ -9,24 +9,24 @@ contract AtomicSwap {
         bool exists;
     }
 
-    // maps the bytes20 hash to a swap    
-    mapping(bytes20 => Swap) private swaps;
+    // maps the redeemer and bytes20 hash to a swap    
+    mapping(address => mapping(bytes20 => Swap)) public swaps;
     
     // creates a new swap
     function initiate(uint _expiration, bytes20 _hash, address _participant) payable public {
-        Swap storage s = swaps[_hash];
+        Swap storage s = swaps[_participant][_hash];
         
         // make sure you aren't overwriting a pre-existing swap
         // (so the original initiator can't rewrite the terms)
         require(s.exists == false);
-        swaps[_hash] = Swap(_expiration, msg.sender, _participant, msg.value, true);
+        swaps[_participant][_hash] = Swap(_expiration, msg.sender, _participant, msg.value, true);
     }
     
     function redeem(bytes32 _secret) public {
         // get a swap from the mapping. we can do it directly because there is no way to 
         // fake the secret.
         bytes20 hash = ripemd160(_secret);
-        Swap storage s = swaps[hash];
+        Swap storage s = swaps[msg.sender][hash];
         
         // make sure it's the right sender
         require(msg.sender == s.participant);
@@ -36,8 +36,8 @@ contract AtomicSwap {
         msg.sender.transfer(s.value);
     }
     
-    function refund(bytes20 _hash) public {
-        Swap storage s = swaps[_hash];
+    function refund(bytes20 _hash, address _participant) public {
+        Swap storage s = swaps[_participant][_hash];
         require(now > s.expiration);
         require(msg.sender == s.initiator);
         
--- contracts/AtomicTokenSwap.sol
+++ contracts/AtomicTokenSwap.sol
@@ -25,11 +25,11 @@ contract AtomicTokenSwap {
     }
 
     // maps the bytes20 hash to a swap    
-    mapping(bytes20 => Swap) private swaps;
+    mapping(address => mapping(bytes20 => Swap)) public swaps;
     
     // creates a new swap
     function initiate(uint _expiration, bytes20 _hash, address _participant, address _token, uint256 _value) public {
-        Swap storage s = swaps[_hash];
+        Swap storage s = swaps[_participant][_hash];
         
         // make sure you aren't overwriting a pre-existing swap
         // (so the original initiator can't rewrite the terms)
@@ -41,14 +41,14 @@ contract AtomicTokenSwap {
         token.transferFrom(msg.sender, this, _value);
 
         // create the new swap
-        swaps[_hash] = Swap(_expiration, msg.sender, _participant, _token, _value, true);
+        swaps[_participant][_hash] = Swap(_expiration, msg.sender, _participant, _token, _value, true);
     }
     
     function redeem(bytes32 _secret) public {
         // get a swap from the mapping. we can do it directly because there is no way to 
         // fake the secret.
         bytes20 hash = ripemd160(_secret);
-        Swap storage s = swaps[hash];
+        Swap storage s = swaps[msg.sender][hash];
         
         // make sure it's the right sender
         require(msg.sender == s.participant);
@@ -59,8 +59,8 @@ contract AtomicTokenSwap {
         token.transfer(msg.sender, s.value);
     }
     
-    function refund(bytes20 _hash) public {
-        Swap storage s = swaps[_hash];
+    function refund(bytes20 _hash, address _participant) public {
+        Swap storage s = swaps[_participant][_hash];
         require(now > s.expiration);
         require(msg.sender == s.initiator);
         
